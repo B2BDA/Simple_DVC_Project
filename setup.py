954b9ed19a42e2c02174b60f384550a9fef408da
B2BDA <bishwarup1429@gmail.com>
2022-07-09 09:14:16 +0530

    creation of basic files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="B2BDA",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/B2BDA/Simple_DVC_Project",
+    author_email="bishwarup1429@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    packages=["src"],
+    license="GNU",
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'dvc[gdrive]',
+        'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
+)
